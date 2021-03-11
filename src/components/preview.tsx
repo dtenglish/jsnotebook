d7430e5a41757173e8125a80472bc43a32aa3cd3
D.T. English <danieltaylorenglish@gmail.com>
2021-03-11 09:38:21 -0800

    improved error handling logic

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -6,23 +6,32 @@ interface PreviewProps {
 }
 
 const html = `
-<html>
-  <head></head>
-  <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message', (event) => {
-        try {
-          eval(event.data);
-        } catch (err) {
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        const handleError = (err) => {
           const root = document.querySelector('#root');
           root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
           throw err;
-        }
-      }, false);
-    </script>
-  </body>
-</html>
+        };
+
+        window.addEventListener('error', () => {
+          event.preventDefault();
+          handleError(event.error);
+        });
+
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            handleError(err);
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
 `;
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
