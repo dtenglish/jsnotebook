69fb1af814435cf24b1c50943ace52e3d2707a99
D.T. English <danieltaylorenglish@gmail.com>
2021-03-15 11:48:58 -0700

    added cell reducer logic

--- packages/local-client/src/state/action-creators/index.ts
+++ packages/local-client/src/state/action-creators/index.ts
@@ -1,1 +1,40 @@
-export { };
+import { ActionType } from '../action-types';
+import { Action, DeleteCellAction, InsertCellAction, MoveCellAction, UpdateCellAction, Direction } from '../actions';
+import { CellTypes } from '../cell';
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id
+  };
+};
+
+export const insertCell = (id: string, type: CellTypes): InsertCellAction => {
+  return {
+    type: ActionType.INSERT_CELL,
+    payload: {
+      id,
+      type
+    }
+  };
+};
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction
+    }
+  };
+};
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content
+    }
+  };
+};
