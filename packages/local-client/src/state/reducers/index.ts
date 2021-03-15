089d6235cd318fdd268333f61ccdb42b9f01751e
D.T. English <danieltaylorenglish@gmail.com>
2021-03-15 10:36:04 -0700

    further redux scaffolding

--- packages/local-client/src/state/reducers/index.ts
+++ packages/local-client/src/state/reducers/index.ts
@@ -1,1 +1,10 @@
-export { };
+import { combineReducers } from 'redux';
+import cellReducer from './cellReducer';
+
+const reducers = combineReducers({
+  cells: cellReducer
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
