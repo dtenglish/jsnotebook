6a605bccf1403717d037d7295c628c91806cbd2d
D.T. English <danieltaylorenglish@gmail.com>
2021-03-18 10:36:09 -0700

    integrated redux into code cell and text editor

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,39 @@
+import { types } from 'jscodeshift';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { ActionType } from './action-types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+  type: ActionType.INSERT_CELL,
+  payload: {
+    id: null,
+    type: 'code'
+  }
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL,
+  payload: {
+    id: null,
+    type: 'text'
+  }
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL,
+  payload: {
+    id: null,
+    type: 'code'
+  }
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL,
+  payload: {
+    id: null,
+    type: 'text'
+  }
+})
