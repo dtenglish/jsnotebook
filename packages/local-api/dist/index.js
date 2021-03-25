ae9c66a31eec54ffc24e278351009062084094d5
D.T. English <danieltaylorenglish@gmail.com>
2021-03-25 12:06:11 -0700

    added CLI error handling

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,9 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 var serve = function (dir, filename, port) {
-    console.log('serving traffic on port', port);
-    console.log('saving/fetching cells from', filename);
-    console.log('file is located in directory:', dir);
+    var app = express_1.default();
+    app.use(http_proxy_middleware_1.createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }));
+    return new Promise(function (resolve, reject) {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
