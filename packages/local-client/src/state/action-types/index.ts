23451251c27dd75ebbf2f7b302f8c9d7b6cf10d6
D.T. English <danieltaylorenglish@gmail.com>
2021-03-26 09:48:49 -0700

    added cell fetching action types

--- packages/local-client/src/state/action-types/index.ts
+++ packages/local-client/src/state/action-types/index.ts
@@ -4,5 +4,8 @@ export enum ActionType {
   MOVE_CELL = 'move_cell',
   UPDATE_CELL = 'update_cell',
   BUNDLE_START = 'bundle_start',
-  BUNDLE_COMPLETE = 'bundle_complete'
+  BUNDLE_COMPLETE = 'bundle_complete',
+  FETCH_CELLS = 'fetch_cells',
+  FETCH_CELLS_COMPLETE = 'fetch_cells_complete',
+  FETCH_CELLS_ERROR = 'fetch_cells_error'
 }
